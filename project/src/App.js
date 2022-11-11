e89d649a1974c776e73c76943825e94a3abc3c49
TuanAnh-P <1tuananhp@gmail.com>
2022-11-11 17:39:51 -0500

    Merge branch 'main' of https://github.com/lyeskara/SOEN341-PROJECT

--- project/src/App.js
+++ project/src/App.js
@@ -6,23 +6,50 @@ import {
 } from './test_image_population/image_populator'
 
 import {
+    Outlet,
     Routes,
     Route,
 } from "react-router-dom";
+
 import Nav from './components/Nav';
+import Header from "./components/Header"
 import Footer from './components/Footer';
+
+import app from './componentCSS/app.css'
 
 import Inventory from './routes/Inventory';
 import Login from './routes/Login';
 import Home from './routes/Home';
 import About from './routes/About';
+
+import Product from "./routes/Product"
+import Cart from "./routes/Cart"
+
 import User from './tables/User';
 import Db_pop from './routes/populate_db';
-import app from './componentCSS/app.css'
 import Order from './tables/Order';
 import Supplier from './tables/Supplier';
 
 let GENERATE_TABLE_ENTRIES = 0; //to generate db data using the generation functions below change 0 to 1
+
+const AppLayout1 = () => {
+    return (
+        <>
+            <Nav/>
+            <Outlet/>
+        </>
+    )
+}
+
+const AppLayout2 = () => {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+        </>
+    )
+}
+
 
 const App = ()=>{
 
@@ -37,24 +64,31 @@ const App = ()=>{
     }
     console.log("supabase testing ends");
 
-    return (   
-       <>
- <Nav/> 
- <Routes>
-    <Route path ="/" element={<Home/>}  />
-    <Route path ="/About" element={<About/>}  />
-    <Route path ="/inventory" element={<Inventory/>}  />
-    <Route path ="/login" element={<Login/>}  />
-    <Route path ="/orders" element={<Order/>}  />
-    <Route path ="/users" element={<User/>}  />
-    <Route path ="/suppliers" element={<Supplier/>}  />
-
-
-
- </Routes>
- </>
     
-    )
+    return (   
+        <>
+           <Routes>
+  
+              <Route element={<AppLayout1/>}>
+                  <Route path ="/" element={<Home/>}  />
+                  <Route path ="/About" element={<About/>}  />
+                  <Route path ="/inventory" element={<Inventory/>}  />
+                  <Route path ="/login" element={<Login/>}  />
+                  <Route path ="/orders" element={<Order/>}  />
+                  <Route path ="/users" element={<User/>}  />
+                  <Route path ="/suppliers" element={<Supplier/>}  />
+              </Route>
+  
+              <Route element={<AppLayout2/>}>
+                 <Route path='/product' element={<Product/>}/>
+                 <Route path='/cart' element={<Cart/>}/>
+              </Route>
+  
+           </Routes>
+        </>
+      
+      )
+
 }
 
 
--- project/src/index.js
+++ project/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Context from './context/Context';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <BrowserRouter>  
-      <App/>       
+   <BrowserRouter> 
+    <Context>
+      <App/> 
+    </Context>      
    </BrowserRouter>
   </React.StrictMode>
 );
