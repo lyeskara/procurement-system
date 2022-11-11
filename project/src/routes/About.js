2010e7e2cd5d22830605d2d9a35642bb83739552
Lyes Kara <lyeskara>
2022-11-11 16:09:46 -0500

    css updates

--- project/src/routes/About.js
+++ project/src/routes/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h2>rgeon</h2>
+      <h2>The goal of this project is to develop a procurement system which automates material acquisition of a business using SQL, React.js and express</h2>
     </div>
   )
 }
