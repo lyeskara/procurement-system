8e82c224f0005aed31cb9d67bc89ada19c0279c8
Radwanbaba13 <68618601+Radwanbaba13@users.noreply.github.com>
2022-12-05 15:59:36 -0500

    Add files via upload

--- project/src/tables/Quotations.js
+++ project/src/tables/Quotations.js
@@ -1,66 +1,60 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { denyOrder } from "../components/CreateOrder";
+import { approveOrder } from "../components/CreateOrder";
 function Quotation() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/quotations").then((Response) => {
+      setData(Response.data);
+    });
+  }, []);
+  return (
+    <div>
+      <div>
+        <h1 id="hh">Quotations waiting for Approval</h1>
 
-
-    const approve = () => {
-
-    }
-    const deny = () => {
-
-
-    }
-    const [data, setData] = useState([]);
-    useEffect(
-        () => {
-            axios.get("http://localhost:5000/quotations")
-                .then((Response) => {
-                    setData(Response.data)
-                })
-        }, []
-    );
-    return (
-        <div>
-
-            <div>
-                <h1 id='hh'>Quotations waiting for Approval</h1>
-
-                <table>
-                    <thead>
-                    <tr>
-
-                        <th>item</th>
-                        <th>supplier</th>
-                        <th>quantity</th>
-                        <th>fulfilment</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        data.map((order) => {
-                            return (
-                                <>
-                                    <tr key={order.id}>
-                                        <td>{order.item_name}</td>
-                                        <td>{order.supplier_name}</td>
-                                        <td>{order.quantity}</td>
-                                        <td>{(order.fulfillment).toString()}</td>
-                                        <button OnClick={approve}>Approve</button>
-                                        <button OnClick={deny}>Deny</button>
-                                    </tr>
-                                </>
-                        )
-                        })
-                    }
-                    </tbody>
-                </table>
-            </div>
-
-
-        </div>
-    )
+        <table>
+          <thead>
+            <tr>
+              <th>item</th>
+              <th>supplier</th>
+              <th>quantity</th>
+              <th>fulfilment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((order) => {
+              return (
+                <>
+                  <tr key={order.id}>
+                    <td>{order.item_name}</td>
+                    <td>{order.supplier_name}</td>
+                    <td>{order.quantity}</td>
+                    <td>{order.fulfillment.toString()}</td>
+                    <button
+                      onClick={() => {
+                        approveOrder(order.id);
+                      }}
+                    >
+                      Approve
+                    </button>
+                    <button
+                      onClick={() => {
+                        denyOrder(order.id);
+                      }}
+                    >
+                      Deny
+                    </button>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Quotation
+export default Quotation;
