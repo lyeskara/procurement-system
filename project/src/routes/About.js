1d4556ce50572c9b991cb605e7cba77f3cb290d0
Thamina <thamina_akther@hotmail.com>
2022-12-09 02:12:11 -0500

    - Home page text CSS updated  - Products page filter CSS updated - Added a CSS file for the about page and updated the text of the about page description

--- project/src/routes/About.js
+++ project/src/routes/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../componentCSS/about.css";
 
 function About() {
   return (
     <div>
-      <h2>The goal of this project is to develop a procurement system which automates material acquisition of a business using SQL, React.js and express</h2>
+      <h2 id='AboutDescription'>The goal of this project is to develop a procurement system which automates material acquisition of a business using SQL, React.js and express</h2>
     </div>
   )
 }
